5f137d823bc912355860aa17ec73cd6ebfe5c01a
Savvy <141248514+SoliditySavvy@users.noreply.github.com>
2023-09-18 01:34:59 +0200

    init

---
+++ README.md
@@ -0,0 +1,46 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/mxICmUeZ)
+
+<div align="center">
+
+# 💜 Autumn School of Solana 2023 💜
+
+</div>
+
+## 👋 Initial Words 👋
+
+Welcome to your **Entry Task** for the **Autumn School of Solana 2023**.
+
+**The objectives of this task are to:**
+- 📝 Evaluate your prior programming experience.
+- 📝 Acquaint you with the task assignment process.
+
+-----
+
+## 📚 Task Details 📚
+
+Inside a GitHub repository, you will find a folder named **`src`**. Within this folder, there's a file named **`main.rs`** that contains **10 functions, each resembling a question**. Currently, each of these functions returns a character set to **`x`**.
+
+### Your task is to `modify` these `characters` in accordance with the provided questions. Once you've made your changes, push them to the repository (main branch). Our team will then automatically evaluate your submitted answers. Please ensure you only push changes made inside the `main.rs` file.
+
+#### 🟥 DO NOT alter any part of the source code other than the return values. Any unintended changes will be your responsibility.
+
+-----
+
+### 👩‍💻 Technical SETUP 👩‍💻
+
+This task is written in the **Rust programming language**. Familiarizing yourself with Rust will be beneficial since it is **the predominant programming language used on the `Solana Blockchain`**. However, for this task, intricate knowledge of Rust is **not mandatory** as you're merely required to change return values. If you wish, you can **install Rust** using the `link provided below` and get acquainted with its associated workflow.
+
+[Install Rust](https://www.rust-lang.org/tools/install)
+
+-----
+
+#### If you've successfully reached this point and answered the given questions:
+#### 👏 `Congratulations` 👏.
+#### Please be patient while we evaluate your submission. We'll get in touch with you soon!
+
+
+-----
+
+# What's next?
+If you're interested in our company, don't hesitate to visit our website, [Ackee Blockchain](https://ackeeblockchain.com), or reach out to us on [Discord](https://discord.gg/x7qXXnGCsa). You can also follow us on [X(Twitter)](https://twitter.com/ackeeblockchain?lang=en) for updates.\
+For the most recent [Solana News](https://solana.com/news) or [Solana Twitter](https://twitter.com/solana).
---
+++ src/main.rs
@@ -0,0 +1,115 @@
+mod tests;
+
+/// *****************************************************************************
+/// *****************************************************************************
+/// *****************************************************************************
+/// ******       EDIT ONLY RETURN CHARACTERS OF QUESTION FUNCTIONS         ******
+/// *****************************************************************************
+/// *****************************************************************************
+/// *****************************************************************************
+#[allow(unused_doc_comments)]
+pub mod questions {
+    pub fn question_1() -> char {
+        /// What is the value of a after executing the following code?
+        ///s
+        /// a = 5
+        /// a = a + 3
+        ///
+        /// a) 3
+        /// b) 5
+        /// c) 8
+        /// d) 10
+        'c'
+    }
+    pub fn question_2() -> char {
+        /// Which of the following is not a valid variable name in Python?
+        ///
+        /// a) _variable
+        /// b) var123
+        /// c) 123var
+        /// d) variable_name
+        'c'
+    }
+    pub fn question_3() -> char {
+        /// What is the output of the following code snippet?
+        ///
+        /// System.out.println(10 / 3);
+        ///
+        /// a) 3
+        /// b) 3.3333
+        /// c) 0
+        /// d) An error is thrown
+        'a'
+    }
+    pub fn question_4() -> char {
+        /// Which data structure uses the Last-In-First-Out (LIFO) principle?
+        ///
+        /// a) Array
+        /// b) Queue
+        /// c) Stack
+        /// d) Linked List
+        'c'
+    }
+    pub fn question_5() -> char {
+        /// How do you comment out multiple lines in a C program?
+        ///
+        /// a) //
+        /// b) /* */
+        /// c) <!-- -->
+        /// d) --
+        'b'
+    }
+    pub fn question_6() -> char {
+        /// What does the "public" keyword in Java signify?
+        ///
+        /// a) The class/method can be accessed from anywhere
+        /// b) The class/method can only be accessed within the class
+        /// c) The class/method can be accessed within the same package
+        /// d) The class/method is private to the package
+        'a'
+    }
+    pub fn question_7() -> char {
+        /// What HTML element is used to create a hyperlink?
+        ///
+        /// a) <hl>
+        /// b) <ahref>
+        /// c) <link>
+        /// d) <a>
+        'a'
+    }
+    pub fn question_8() -> char {
+        /// What will the following code print in JavaScript?
+        ///
+        /// console.log("10" + 5);
+        ///
+        /// a) 15
+        /// b) "105"
+        /// c) "15"
+        /// d) An error is thrown
+        'b'
+    }
+    pub fn question_9() -> char {
+        /// Which SQL command is used to retrieve data from a database?
+        ///
+        /// a) INSERT
+        /// b) DELETE
+        /// c) UPDATE
+        /// d) SELECT
+        'd'
+    }
+    pub fn question_10() -> char {
+        /// What is the correct way to declare a variable in Python?
+        ///
+        /// a) var x = 10;
+        /// b) int x = 10;
+        /// c) x: int = 10;
+        /// d) x = 10
+        'd'
+    }
+}
+/// *****************************************************************************
+/// *****************************************************************************
+/// *****************************************************************************
+/// *****************************************************************************
+/// *****************************************************************************
+fn main() {}
